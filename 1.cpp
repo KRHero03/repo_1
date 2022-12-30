5d8d28499549a301005d20b93c02e4476f55bcb0
dhruvirank0105 <dhruvirank2005@gmail.com>
2022-12-30 13:27:32 +0530

    Added comment

--- 1.cpp
+++ 1.cpp
@@ -1,6 +1,7 @@
 // Write code to display numbers from 1 to 5 in cpp.
 #include <bits/stdc++.h>
 using namespace std ;
+// abcdef
 int main(){
     for(int i = 1 ; i <=5 ; i++) {
         cout<<i<<" ";
